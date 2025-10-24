2651effc40b8f2e2d37ed22a041169a22088d4d6
Aswin Reji <aswinreji.004@gmail.com>
2025-10-24 17:21:44 +0000

    feat: we added folderStructure and update fileTree for each folder renderTree for file and fodler

--- src/components/TreeFileStructure/fileTree.jsx
+++ src/components/TreeFileStructure/fileTree.jsx
@@ -1,25 +1,46 @@
 import { useState } from "react";
 
-const FileTree = ({ tree, onSelectFile , activeFile}) => {
-    const [isOpen, setIsOpen] = useState(true);
+const FileTree = ({ tree, onSelectFile, activeFile }) => {
     const renderTree = (nodes) =>
-
         nodes.map((node) => (
             <div key={node.name} className="ml-2">
                 {node.type === 'folder' ? (
-                    <div>
-                        <span className="font-bold"> {node.name} </span>
-                        <div className="ml-4"> {node.children && renderTree(node.children)} </div>
-                    </div>
+                    <FolderNode node={node} />
                 ) : (
-                    <div className="cursor-pointer hover:bg-gray-200 rounded p-1"
-                        onClick={() => onSelectFile(node)}
-                    >
-                        {node.name}
-                    </div>
+                    <FileNode node={node} />
                 )}
             </div>
         ))
+
+    const FolderNode = ({ node }) => {
+        const [isOpen, setIsOpen] = useState(true);
+        return (
+            <div>
+                <div className="font-bold cursor-pointer flex items-center"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    {isOpen ? "📂" : "📁"} {node.name}
+                </div>
+                {
+                    isOpen && node.children && (
+                        <div className="ml-4">
+                            {renderTree(node.children)}
+                        </div>
+                    )
+                }
+            </div>
+        )
+    }
+
+    const FileNode = ({ node }) => {
+        <div className={`cursor-pointer rounded p-1 ${activeFile?.name === node.name ? "bg-blue-300" : "hover:bg-gray-200"
+            }`}
+            onClick={onSelectFile(node)}
+        >
+            📄 {node.name}
+        </div>
+    }
+
     return (
         <div>
             {renderTree(tree)}
